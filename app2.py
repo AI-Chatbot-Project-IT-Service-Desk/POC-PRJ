0fe886aadbbf96b15501af13dd43438dfa128a16
Tae Young <49816869+teon98@users.noreply.github.com>
2024-08-23 15:05:59 +0900

    Streamlit Navbar에서 같은 페이지가 2번 호출되어 unique 값이 고유하지 않다는 에러 픽스

--- app2.py
+++ app2.py
@@ -1,29 +1,15 @@
 import streamlit as st
 from menu import menu
 
-# Initialize st.session_state.role to None
+# Initialize st.session_state.role to "사용자"
 if "role" not in st.session_state:
     st.session_state.role = "사용자"
 
 # 관리자로서 로그인 인증을 완료했는지 여부
 if 'authenticated' not in st.session_state:
-        st.session_state.authenticated = False
+    st.session_state.authenticated = False
 
-# Retrieve the role from Session State to initialize the widget
 st.session_state._role = st.session_state.role
 
-# 관리자 비밀번호 설정
-ADMIN_PASSWORD = "0000"  
-
-def set_role():
-    st.session_state.role = st.session_state._role
-    
-
-#Selectbox to choose role
-st.sidebar.selectbox(
-    "Select your role", 
-    ["사용자", "관리자"],
-    key="_role",
-    on_change=set_role
-)
 menu()
+st.switch_page("pages/user_CescoBotHome.py")
--- menu.py
+++ menu.py
@@ -1,29 +1,46 @@
 import streamlit as st
 
-def authenticated_menu():
+def user_menu():
     # Show a navigation menu for authenticated users
     st.sidebar.page_link("pages/user_CescoBotHome.py", label="🤖 채팅 시작하기")
     st.sidebar.page_link("pages/user_ManualList.py", label="📃 매뉴얼 열람하기")
-    if st.session_state.role == "관리자":
-        st.sidebar.page_link("pages/admin_OriginDataManage.py", label="📀 매뉴얼 원본 데이터 관리")
-        st.sidebar.page_link("pages/admin_SplitDataManage.py", label="💿 매뉴얼 데이터 관리",)
-        st.sidebar.page_link("pages/admin_UnAnswerd.py", label="📊 미응답 데이터 관리")
+    st.sidebar.page_link("pages/admin_Login.py", label="🔒 관리자 계정 로그인")
 
-def unauthenticated_menu():
-    # Show a navigation menu for unauthenticated users
-    st.sidebar.page_link("app2.py", label="관리자 계정 로그인")
+def admin_menu():
+    #st.sidebar.page_link("pages/user_CescoBotHome.py", label="🤖 채팅 시작하기")
+    #st.sidebar.page_link("pages/user_ManualList.py", label="📃 매뉴얼 열람하기")
+    st.sidebar.page_link("pages/admin_OriginDataManage.py", label="📀 매뉴얼 원본 데이터 관리")
+    st.sidebar.page_link("pages/admin_SplitDataManage.py", label="💿 매뉴얼 데이터 관리",)
+    st.sidebar.page_link("pages/admin_UnAnswerd.py", label="📊 미응답 데이터 관리")
+
+
+def set_role():
+    if st.session_state._role == "관리자":
+        if not st.session_state.authenticated:
+            st.session_state._role = "사용자"
+            st.toast("관리자 인증이 필요합니다", icon="⚠️")
+            return
+        
+    st.session_state.role = st.session_state._role
+
+def select_role():
+    st.sidebar.selectbox(
+        "Select your role:",
+        ["사용자", "관리자"],
+        key="_role",
+        on_change=set_role,
+    )
 
 def menu():
-    # Determine if a user is logged in or not, then show the correct
-    # navigation menu
-    if "role" not in st.session_state or st.session_state.role is "사용자":
-        unauthenticated_menu()
-        return
-    authenticated_menu()
+    select_role()
+    if st.session_state.role == "사용자":
+        user_menu()
+    elif st.session_state.role == "관리자":
+        admin_menu()
 
 def menu_with_redirect():
-    # Redirect users to the main page if not logged in, otherwise continue to
-    # render the navigation menu
     if "role" not in st.session_state or st.session_state.role is None:
         st.switch_page("app2.py")
+
+    st.session_state._role = st.session_state.role
     menu()
